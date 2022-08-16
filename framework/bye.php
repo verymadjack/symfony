492d6fc9ad7953fe40105f4bb5569bbd3406c24c
verymadjack <verymadjacek@gmail.com>
2022-08-16 17:48:28 +0300

    Symfony learning 1st commit

---
+++ framework/bye.php
@@ -0,0 +1,6 @@
+<?php
+// framework/bye.php
+require_once __DIR__.'/init.php';
+
+$response->setContent('Goodbye!');
+$response->send();
--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$name = $_GET['name'] ?? 'World';
-
-header('Content-Type: text/html; charset=utf-8');
-
-printf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
